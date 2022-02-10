421b09eb6cfc10b28cfced7719952f779235a2d9
musafir928 <uganoghli@gmail.com>
2022-02-10 23:58:54 +0100

    caesar cipher

---
+++ symmetric/Caesar_Cipher.py
@@ -0,0 +1,22 @@
+ALPHABET = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+KEY = 100
+
+
+# this algorithm uses index of each letter and adding a key value to the index
+# so that plain text has been en and decrypted
+
+
+def caesar_crypto(text, target):
+    global KEY
+    result = ""
+    if target == "de":
+        KEY = -KEY
+    for c in text.upper():
+        index = ALPHABET.find(c)
+        result += c if index == -1 else ALPHABET[(index + KEY) % 26]
+    return result
+
+
+print(caesar_crypto(caesar_crypto("men bir uyghur!!", "en"), "de"))
+
+# TODO: convert to JAVA
